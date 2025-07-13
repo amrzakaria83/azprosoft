0e4377ea808f51ca048c7cfcee7faed0ac924119
amrzakaria83 <133797388+amrzakaria83@users.noreply.github.com>
2025-07-14 00:37:31 +0300

    df

---
+++ database/migrations/2025_07_14_002205_create_pur_imports_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pur_imports', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_id')->nullable();
+            $table->string('quantity')->nullable();
+            $table->string('note')->nullable();
+            $table->tinyInteger('status')->default(0)->comment("0 = active - 1 = not active ");
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pur_imports');
+    }
+};
