044963ebbb66302cbdea9cd3a99260b7064d4452
amrzakaria83 <pharmastyl@gmail.com>
2025-07-20 22:42:42 +0300

    df

--- resources/views/admin/employee/addemanger.blade.php
+++ resources/views/admin/employee/addemanger.blade.php
@@ -36,110 +36,110 @@
 @section('content')
     <div id="kt_app_content_container" class="app-container container-fluid">
 
-                    <div class="card mb-5 mb-xl-10">
-                        <!--begin::Content-->
-                        <div id="kt_account_settings_profile_details" class="collapse show">
-                        <form action="{{route('admin.employees.store')}}" method="POST" enctype="multipart/form-data" id="kt_account_profile_details_form" class="form" autocomplete="off">
+            <div class="card mb-5 mb-xl-10">
+                <!--begin::Content-->
+                <div id="kt_account_settings_profile_details" class="collapse show">
+                    <form action="{{route('admin.employees.store')}}" method="POST" enctype="multipart/form-data" id="kt_account_profile_details_form" class="form" autocomplete="off">
                         @csrf
-                                <!--begin::Card body-->
-                                <div class="card-body border-top " >
-                                    <input type="hidden" name="id" value="{{$data->id}}" />
-                                    
-                                    <div class="row mb-6">
-                                        <label class="col-lg-2 col-form-label required fw-semibold fs-6 ">{{trans('lang.name_ar')}}</label>
-                                        <div class="col-lg-8 fv-row">
-                                            <input type="text" id="name_ar" name="name_ar" placeholder="{{trans('lang.name_ar')}}" value="{{$data->emp_name}}" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0 text-center" />
-                                        </div>
-                                    </div>
-                                    <div class="row mb-6">
-                                        <label class="col-lg-2 col-form-label  fw-semibold fs-6">{{trans('lang.name_en')}}</label>
-                                        <div class="col-lg-8 fv-row">
-                                            <input type="text" id="name_en" name="name_en" placeholder="{{trans('lang.name_en')}}" value="{{$data->emp_name_en}}" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0 text-center" />
-                                        </div>
-                                    </div>
-                                    <div class="row mb-6">
-                                        <label class="col-lg-2 col-form-label required fw-semibold fs-6">
-                                        {{trans('lang.phone')}}
-                                        </label>
-                                        <div class="col-lg-8 fv-row">
-                                            <input type="text" name="phone" placeholder="{{trans('lang.phone')}}" id="phone" value="{{$data->emp_tell}}" class="form-control form-control-lg form-control-solid text-center" />
-                                        </div>
-                                        
-                                    </div>
-                                    <div class="row mb-6">
-                                        <label class="col-lg-2 col-form-label required fw-semibold fs-6">{{trans('lang.email')}}</label>
-                                        <div class="col-lg-8 fv-row">
-                                            <input type="text" required id="emailaz" name="emailaz" placeholder="{{trans('lang.email')}}" value="null" class="bg bg-danger text-dark form-control form-control-lg form-control-solid text-center" />
-                                        </div>
-                                    </div>
-                                    
-                                    <div class="row mb-6">
-                                        <label class="col-lg-2 col-form-label fw-semibold fs-6 required">
-                                        {{trans('lang.password')}}
-                                            <i class="fas fa-exclamation-circle ms-1 fs-7" data-bs-toggle="tooltip" title="لا يقل عن 6 حروف"></i>
-                                        </label>
-                                        <div class="col-lg-8 fv-row">
-                                            <input type="password" name="password" placeholder="{{trans('lang.password')}}" value="null" class="bg bg-danger text-dark form-control form-control-lg form-control-solid text-center" />
-                                        </div>
-                                    </div>
-                                    
-                                    
-                                    <div class="row mb-6">
-                                        <label class="col-sm-2 col-form-label required fw-semibold fs-6" >type</label>
-                                        <div class="col-sm-2 d-flex align-items-center">
-                                            <select  class="form-select form-select-lg form-select-solid text-center" id="type" name="type" value="" >
-                                                <option value="0">Dash</option>   <!-- admin -->
-                                                <option value="1" selected>No dash</option>
-                                                <option value="2">Delivery access</option>  <!-- subadmin -->
-                                                <option value="3" disabled></option> <!-- admin -->
-                                            </select>
-                                        </div>
-                                        <label class="col-sm-2 col-form-label required fw-semibold fs-6" style=" max-width: 7% !important;">is_active</label>
-                                        <div class="col-sm-2 d-flex align-items-center">
-                                            <select  class="form-select form-select-lg form-select-solid text-center" id="is_active"  name="is_active" value="" >
-                                                <option value="1">{{trans('lang.active')}}</option>
-                                                <option value="0">{{trans('lang.inactive')}}</option>
-                                                <option value="2">{{trans('lang.suspended')}}</option>
-                                                <option value="3">{{trans('lang.terminated')}}</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    
-                                    <div class="row mb-6">
-                                        <label class="col-lg-2 col-form-label required fw-semibold fs-3 text-info">{{trans('lang.name_ar')}} </label>
-                                        <div class="col-lg-8 fv-row">
-                                            <select  data-control="select2" data-placeholder="Select an option" class=" input-text form-control  form-select  mb-3 mb-lg-0" id="emangeremp_id" name="emangeremp_id">
-                                            <option value="">Select {{trans('lang.name_ar')}}</option>
-                                                @foreach (App\Models\Emangeremp::get() as $item)
-                                                    <option value="{{$item->emp_id}}" @if(isset($data) && $data->emp_id == $item->emp_id) selected @endif>{{$item->emp_name}}</option>
-                                                @endforeach
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="row mb-6">
-                                        <label class="col-lg-2 col-form-label required fw-semibold fs-3 text-info">{{trans('lang.role_id')}} </label>
-                                        <div class="col-lg-8 fv-row">
-                                            <select  data-control="select2" data-placeholder="Select an option" required class=" input-text form-control  form-select  mb-3 mb-lg-0"  name="role_id">
-                                            <option value="">Select {{trans('lang.role_id')}}</option>
-                                                @foreach (Spatie\Permission\Models\Role::all() as $item=>$row)
-                                                    <option value="{{$row->id}}">{{$row->name}}</option>
-                                                @endforeach
-                                            </select>
-                                        </div>
-                                    </div>
-                                   
-                                    <div class="row mb-6">
-                                        <label class="col-lg-2 col-form-label  fw-semibold fs-6">{{trans('lang.note')}}</label>
-                                        <div class="col-lg-8 fv-row">
-                                            <input type="text" id="note" name="note" placeholder="{{trans('lang.note')}}" value="" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" />
-                                        </div>
-                                    </div>
+                        <!--begin::Card body-->
+                        <div class="card-body border-top " >
+                            <input type="hidden" name="id" value="{{$data->id}}" />
+                            
+                            <div class="row mb-6">
+                                <label class="col-lg-2 col-form-label required fw-semibold fs-6 ">{{trans('lang.name_ar')}}</label>
+                                <div class="col-lg-8 fv-row">
+                                    <input type="text" id="name_ar" name="name_ar" placeholder="{{trans('lang.name_ar')}}" value="{{$data->emp_name}}" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0 text-center" />
                                 </div>
-                                <div class="card-footer d-flex justify-content-end py-6 px-9">
-                                    <button type="submit" class="btn btn-primary" id="kt_account_profile_details_submit">حفظ</button>
+                            </div>
+                            <div class="row mb-6">
+                                <label class="col-lg-2 col-form-label  fw-semibold fs-6">{{trans('lang.name_en')}}</label>
+                                <div class="col-lg-8 fv-row">
+                                    <input type="text" id="name_en" name="name_en" placeholder="{{trans('lang.name_en')}}" value="{{$data->emp_name_en}}" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0 text-center" />
                                 </div>
-                                <!--end::Actions-->
-                            </form>
+                            </div>
+                            <div class="row mb-6">
+                                <label class="col-lg-2 col-form-label required fw-semibold fs-6">
+                                {{trans('lang.phone')}}
+                                </label>
+                                <div class="col-lg-8 fv-row">
+                                    <input type="text" name="phone" placeholder="{{trans('lang.phone')}}" id="phone" value="{{$data->emp_tell}}" class="form-control form-control-lg form-control-solid text-center" />
+                                </div>
+                                
+                            </div>
+                            <div class="row mb-6">
+                                <label class="col-lg-2 col-form-label required fw-semibold fs-6">{{trans('lang.email')}}</label>
+                                <div class="col-lg-8 fv-row">
+                                    <input type="text" required id="emailaz" name="emailaz" placeholder="{{trans('lang.email')}}" value="null" class="bg bg-danger text-dark form-control form-control-lg form-control-solid text-center" />
+                                </div>
+                            </div>
+                            
+                            <div class="row mb-6">
+                                <label class="col-lg-2 col-form-label fw-semibold fs-6 required">
+                                {{trans('lang.password')}}
+                                    <i class="fas fa-exclamation-circle ms-1 fs-7" data-bs-toggle="tooltip" title="لا يقل عن 6 حروف"></i>
+                                </label>
+                                <div class="col-lg-8 fv-row">
+                                    <input type="password" name="password" placeholder="{{trans('lang.password')}}" value="null" class="bg bg-danger text-dark form-control form-control-lg form-control-solid text-center" />
+                                </div>
+                            </div>
+                            
+                            
+                            <div class="row mb-6">
+                                <label class="col-sm-2 col-form-label required fw-semibold fs-6" >type</label>
+                                <div class="col-sm-2 d-flex align-items-center">
+                                    <select  class="form-select form-select-lg form-select-solid text-center" id="type" name="type" value="" >
+                                        <option value="0">Dash</option>   <!-- admin -->
+                                        <option value="1" selected>No dash</option>
+                                        <option value="2">Delivery access</option>  <!-- subadmin -->
+                                        <option value="3" disabled></option> <!-- admin -->
+                                    </select>
+                                </div>
+                                <label class="col-sm-2 col-form-label required fw-semibold fs-6" style=" max-width: 7% !important;">is_active</label>
+                                <div class="col-sm-2 d-flex align-items-center">
+                                    <select  class="form-select form-select-lg form-select-solid text-center" id="is_active"  name="is_active" value="" >
+                                        <option value="1">{{trans('lang.active')}}</option>
+                                        <option value="0">{{trans('lang.inactive')}}</option>
+                                        <option value="2">{{trans('lang.suspended')}}</option>
+                                        <option value="3">{{trans('lang.terminated')}}</option>
+                                    </select>
+                                </div>
+                            </div>
+                            
+                            <div class="row mb-6">
+                                <label class="col-lg-2 col-form-label required fw-semibold fs-3 text-info">{{trans('lang.name_ar')}} </label>
+                                <div class="col-lg-8 fv-row">
+                                    <select  data-control="select2" data-placeholder="Select an option" class=" input-text form-control  form-select  mb-3 mb-lg-0" id="emangeremp_id" name="emangeremp_id">
+                                    <option value="">Select {{trans('lang.name_ar')}}</option>
+                                        @foreach (App\Models\Emangeremp::get() as $item)
+                                            <option value="{{$item->emp_id}}" @if(isset($data) && $data->emp_id == $item->emp_id) selected @endif>{{$item->emp_name}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row mb-6">
+                                <label class="col-lg-2 col-form-label required fw-semibold fs-3 text-info">{{trans('lang.role_id')}} </label>
+                                <div class="col-lg-8 fv-row">
+                                    <select  data-control="select2" data-placeholder="Select an option" required class=" input-text form-control  form-select  mb-3 mb-lg-0"  name="role_id">
+                                    <option value="">Select {{trans('lang.role_id')}}</option>
+                                        @foreach (Spatie\Permission\Models\Role::all() as $item=>$row)
+                                            <option value="{{$row->id}}">{{$row->name}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                           
+                            <div class="row mb-6">
+                                <label class="col-lg-2 col-form-label  fw-semibold fs-6">{{trans('lang.note')}}</label>
+                                <div class="col-lg-8 fv-row">
+                                    <input type="text" id="note" name="note" placeholder="{{trans('lang.note')}}" value="" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer d-flex justify-content-end py-6 px-9">
+                            <button type="submit" class="btn btn-primary" id="kt_account_profile_details_submit">حفظ</button>
+                        </div>
+                        <!--end::Actions-->
+                    </form>
                 <!--end::Form-->
             </div>
         </div>
--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -139,6 +139,164 @@
                   </li>
                 </ul>
               </li>
+              <!-- overtime Submenu -->
+              <li class="dropdown-submenu dropend">
+                <a class="dropdown-item dropdown-toggle py-3" href="#" id="overtimeDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-offset="0,8">
+                  <span class="menu-icon me-3">
+                    <i class="fa-solid fa-sitemap"></i>
+                  </span>
+                  <span class="menu-title fs-3">{{trans('lang.overtime')}}</span>
+                  <span class="menu-arrow"></span>
+                </a>
+                <ul class="dropdown-menu dropdown-submenu-lg" aria-labelledby="overtimeDropdown">
+                  <li>
+                    <a class="dropdown-item py-3" href="{{route('admin.emp_att_overtimes.index')}}">
+                      <span class="menu-icon me-3">
+                        <i class="fa-solid fa-list-check"></i>
+                      </span>
+                      <span class="menu-title fs-3">{{trans('lang.requests')}} {{trans('lang.overtime')}}</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item py-3" href="{{route('admin.emp_att_overtimes.create_bymanger')}}">
+                      <span class="menu-icon me-3">
+                        <i class="fa-solid fa-list-check"></i>
+                      </span>
+                      <span class="menu-title fs-3">{{trans('lang.add')}} {{trans('lang.overtime')}}</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <!-- Permissions Submenu -->
+              <li class="dropdown-submenu dropend">
+                <a class="dropdown-item dropdown-toggle py-3" href="#" id="permissionDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-offset="0,8">
+                  <span class="menu-icon me-3">
+                    <i class="fa-solid fa-sitemap"></i>
+                  </span>
+                  <span class="menu-title fs-3">{{trans('lang.permission_atts')}}</span>
+                  <span class="menu-arrow"></span>
+                </a>
+                <ul class="dropdown-menu dropdown-submenu-lg" aria-labelledby="permissionDropdown">
+                  <li>
+                    <a class="dropdown-item py-3" href="{{route('admin.emp_att_permissions.index')}}">
+                      <span class="menu-icon me-3">
+                        <i class="fa-solid fa-list-check"></i>
+                      </span>
+                      <span class="menu-title fs-3">{{trans('lang.requests')}} {{trans('lang.permission_att')}}</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item py-3" href="{{route('admin.emp_att_permissions.indexall')}}">
+                      <span class="menu-icon me-3">
+                        <i class="fa-solid fa-list-check"></i>
+                      </span>
+                      <span class="menu-title fs-3">{{trans('lang.permission_atts')}}</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <!-- Vacation & Permissions Section -->
+              <li class="dropdown-submenu dropend">
+                <a class="dropdown-item dropdown-toggle py-3" href="#" id="vacationDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-offset="0,8">
+                  <span class="menu-icon me-3">
+                    <i class="fa-solid fa-calendar-days"></i>
+                  </span>
+                  <span class="menu-title fs-3">{{trans('lang.vacation')}}</span>
+                  <span class="menu-arrow"></span>
+                </a>
+                <ul class="dropdown-menu dropdown-submenu-lg" aria-labelledby="vacationDropdown">
+                  <li>
+                    <a class="dropdown-item py-3" href="{{route('admin.vacationemps.create')}}">
+                      <span class="menu-icon me-3">
+                        <i class="fa-solid fa-paper-plane"></i>
+                      </span>
+                      <span class="menu-title fs-3">{{trans('lang.request')}} {{trans('lang.vacation')}}</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item py-3" href="{{route('admin.vacationemps.index')}}">
+                      <span class="menu-icon me-3">
+                        <i class="fa-solid fa-list-check"></i>
+                      </span>
+                      <span class="menu-title fs-3">{{trans('lang.requests')}} {{trans('lang.vacation')}}</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item py-3" href="{{route('admin.vacationemps.indexall')}}">
+                      <span class="menu-icon me-3">
+                        <i class="fa-solid fa-list-check"></i>
+                      </span>
+                      <span class="menu-title fs-3">{{trans('lang.all')}} {{trans('lang.vacations')}}</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              
+              <!-- Rewards & Penalties Section -->
+              <li class="dropdown-submenu dropend">
+                <a class="dropdown-item dropdown-toggle py-3" href="#" id="penaltiesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-offset="0,8">
+                  <span class="menu-icon me-3">
+                    <i class="fa-solid fa-scale-balanced"></i>
+                  </span>
+                  <span class="menu-title fs-3">{{trans('lang.penalties')}} & {{trans('lang.rewards')}}</span>
+                  <span class="menu-arrow"></span>
+                </a>
+                <ul class="dropdown-menu dropdown-submenu-lg" aria-labelledby="penaltiesDropdown">
+                  <li>
+                    <a class="dropdown-item py-3" href="{{route('admin.emp_actions.create')}}">
+                      <span class="menu-icon me-3">
+                        <i class="fa-solid fa-plus-circle"></i>
+                      </span>
+                      <span class="menu-title fs-3">{{trans('lang.add')}} {{trans('lang.action')}}</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item py-3" href="{{route('admin.emp_actions.index')}}">
+                      <span class="menu-icon me-3">
+                        <i class="fa-solid fa-list"></i>
+                      </span>
+                      <span class="menu-title fs-3">{{trans('lang.all')}} {{trans('lang.actions')}}</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              
+              <!-- Salary Management Section -->
+              <li class="dropdown-submenu dropend">
+                <a class="dropdown-item dropdown-toggle py-3" href="#" id="salarysDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-offset="0,8">
+                  <span class="menu-icon me-3">
+                    <i class="fa-solid fa-money-bill-wave"></i>
+                  </span>
+                  <span class="menu-title fs-3">{{trans('lang.salary')}}</span>
+                  <span class="menu-arrow"></span>
+                </a>
+                <ul class="dropdown-menu dropdown-submenu-lg" aria-labelledby="salarysDropdown">
+                  <li>
+                    <a class="dropdown-item py-3" href="{{route('admin.emp_salarys.create')}}">
+                      <span class="menu-icon me-3">
+                        <i class="fa-solid fa-plus-circle"></i>
+                      </span>
+                      <span class="menu-title fs-3">{{trans('lang.add')}} {{trans('lang.salary')}}</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item py-3" href="{{route('admin.emp_salarys.index')}}">
+                      <span class="menu-icon me-3">
+                        <i class="fa-solid fa-list"></i>
+                      </span>
+                      <span class="menu-title fs-3">{{trans('lang.view')}} {{trans('lang.salaries')}}</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item py-3" href="{{route('admin.emp_monthly_salarys.create_m_sa')}}">
+                      <span class="menu-icon me-3">
+                        <i class="fa-solid fa-calendar"></i>
+                      </span>
+                      <span class="menu-title fs-3">{{trans('lang.month')}} {{trans('lang.salary')}}</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
               
             </ul>
           </li>
